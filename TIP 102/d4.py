5f1646d113227860830a508352c60f5f46c0256e
Steven Ren <stevenren395@gmail.com>
2026-06-16 17:57:53 -0400

    day 4 work

---
+++ TIP 102/d4.py
@@ -0,0 +1,27 @@
+# 6/11/26 Set
+
+# Notes:
+empty_dict = {} 
+empty_set = set()
+
+nums = {1,2,3}
+nums_ = {4,5,6}
+# print(nums)
+# nums.remove(1)
+# nums.remove(5) #keyError
+nums.discard(5) #without raising keyError
+# print(nums | nums_)
+# print(nums & nums_)
+# print(nums - nums_)
+# print(nums ^ nums_)
+
+"""
+Set1, #1
+UNDERSTAND:
+> Input:
+> Output:
+> Edge cases:
+
+PLAN:
+
+"""
